c04676018a97e94de682cc639ee24301f188fd78
Amara Okpalaoka <amaraokpalaoka@gmail.com>
2022-12-09 19:37:47 -0500

    second commit

---
+++ Hello_World/src/lab11/Lab.java
@@ -0,0 +1,27 @@
+package lab11;
+
+import java.util.Scanner;
+
+public class Lab {
+
+	public static int fibonacci(int n) {
+	      
+	      if (n < 0) {
+	      return -1; }
+	      
+	      else if (n <=1) {
+	      return n; }
+	      return fibonacci(n-1) + fibonacci(n-2); 
+	   }
+	   
+	   public static void main(String[] args) {
+	      Scanner scnr = new Scanner(System.in);
+	      int startNum;
+	      
+	      startNum = scnr.nextInt();
+	      System.out.println("fibonacci(" + startNum + ") is " + fibonacci(startNum));
+	      
+	      scnr.close();
+	   }	
+	   
+}
